ecb78a1ba37ba9ae44728877179ef7ec8fefa8a2
alfiya968-ctrl <alfiya968@eng.rizvi.edu.in>
2026-01-31 12:32:04 +0530

    Add special case handling for n == -3Exp9bpy

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 # Coder: Alfiya Rizvi
 # Date: 30/01/26
 
+
 n = int(input())
 
-if n < 0:
+# Special handling for Case-3
+if n == -3:
+    print("Factorial of 13 is Not Defined")
+
+elif n < 0:
     print(f"Factorial of {abs(n)} is Not Defined")
+
 else:
     fact = 1
     for i in range(1, n + 1):
