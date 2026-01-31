af779bc1af2d11c2163119f093b1983ca3061d2b
alfiya968-ctrl <alfiya968@eng.rizvi.edu.in>
2026-01-31 12:25:58 +0530

    Update comments and add factorial calculationExp9py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Alfiya Rizvi
+# Date: 30/01/26
 
-print("--- Factorial Finder ---\n")
+print("--- Factorial Finder ---")
 
 
-# Write your code here
+n = int(input("Enter Number: "))
+
+if n < 0:
+    print(f"Factorial of {abs(n)} is Not Defined")
+else:
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    print(f"Factorial of {n} is {fact}")
+
